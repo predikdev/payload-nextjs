67f489bc623f65e93638e50d287f534be427ab21
predikdev <social.predik@gmail.com>
2023-09-10 11:07:38 +0200

    sass -> css edited

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+const path = require('path');
 require('dotenv').config();
 const { sizes } = require('./blocks/Image/sizes.json');
 
 module.exports = {
   publicRuntimeConfig: {
     SERVER_URL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     domains: [
